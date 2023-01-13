4869cde46493f9c08be26ffb156a3eb598c54246
Lucas Paiva <lucasp4iva@gmail.com>
2023-01-12 23:06:30 -0300

    Feat: adding validation to body params

--- MoviesApi/Models/Movie.cs
+++ MoviesApi/Models/Movie.cs
@@ -1,8 +1,15 @@
-﻿namespace MoviesApi.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MoviesApi.Models;
 
 public class Movie
 {
+    [Required(ErrorMessage = "The title is required.")]
     public string Title {  get; set; }
+    [Required(ErrorMessage = "The genre is required.")]
+    [MaxLength(50, ErrorMessage = "Max size of genre is 50 caracters")]
     public string Genre { get; set; }
+    [Required]
+    [Range(70, 300, ErrorMessage = "Duration must be between 70 min and 300 min.")]
     public int Duration { get; set; }
 }
