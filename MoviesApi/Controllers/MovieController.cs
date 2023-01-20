760c945775550309535aa5c49a781a7cd8056147
Lucas Paiva <lucasp4iva@gmail.com>
2023-01-20 19:45:18 -0300

    refactor: change MovieController to use database directely

--- MoviesApi/Controllers/MovieController.cs
+++ MoviesApi/Controllers/MovieController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Filters;
+using MoviesApi.Data;
 using MoviesApi.Models;
 
 namespace MoviesApi.Controllers
@@ -8,27 +9,30 @@ namespace MoviesApi.Controllers
     [Route("[controller]")]
     public class MovieController : ControllerBase
     {
-        private static List<Movie> movies = new List<Movie>();
-        private static int id = 0;
+        private MovieContext _context;
+
+        public MovieController(MovieContext context)
+        {
+            _context = context;
+        }
 
         [HttpPost]
         public IActionResult AddMovie([FromBody] Movie movie)
         {
-            movies.Add(movie);
-            movie.Id = id++;
+            _context.Movies.Add(movie);
             return CreatedAtAction(nameof(GetMovieById), new {id = movie.Id}, movie);
         }
 
         [HttpGet]
         public IEnumerable<Movie> GetMovies([FromQuery] int skip = 0,[FromQuery] int take = 20)
         {
-            return movies.Skip(skip).Take(take)
+            return _context.Movies.Skip(skip).Take(take)
 ;       }
 
         [HttpGet("{id}")]
         public IActionResult GetMovieById(int id)
         {
-            var movie = movies.FirstOrDefault(movie => movie.Id == id);
+            var movie = _context.Movies.FirstOrDefault(movie => movie.Id == id);
             return movie != null ? Ok(movie) : NotFound();
         }
     }
